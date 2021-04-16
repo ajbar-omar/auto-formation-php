d5e346852d6d287cb833a04aa70cbfd15d476b3c
ajbar-omar <ajbar.omar.dev@gmail.com>
2021-04-16 10:32:35 +0000

    .Auto formation

---
+++ 12-functions/functions.php
@@ -0,0 +1,25 @@
+ 
+
+<?php
+$nom = 'Sandra';
+echo 'Bonjour, ' . $nom . ' !<br />';
+
+$nom = 'Patrick';
+echo 'Bonjour, ' . $nom . ' !<br />';
+
+$nom = 'Claude';
+echo 'Bonjour, ' . $nom . ' !<br />';
+
+function DireBonjour($nom)
+{
+    echo 'Bonjour ' . $nom . ' !<br />';
+}
+
+DireBonjour('Marie');
+DireBonjour('Patrice');
+DireBonjour('Edouard');
+DireBonjour('Pascale');
+DireBonjour('François');
+DireBonjour('Benoît');
+DireBonjour('Père Noël');
+?>
